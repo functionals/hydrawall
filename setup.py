b0cc8305168fc1ed7592fabc565ba7df2d20a9d0
functionals <ijmalloy@ole.augie.edu>
2024-08-16 06:29:06 -0500

    update

--- setup.py
+++ setup.py
@@ -4,22 +4,53 @@ Created on Wed Aug 14 15:26:08 2024
 
 @author: Ian Malloy
 """
+from setuptools import find_packages, setup
 
-'import lib py and lib pl'
-
-"make all in lib py"
-
+setup(
+    name="Dshell",
+    version="3.2.3",
+    author="USArmyResearchLab",
+    description="An extensible network forensic analysis framework",
+    url="https://github.com/USArmyResearchLab/Dshell",
+    python_requires='>=3.8',
+    packages=find_packages(),
+    package_data={
+        "dshell": ["data/dshellrc", "data/GeoIP/readme.txt"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console",
+        "Topic :: Security",
+    ],
+    install_requires=[
+        "geoip2",
+        "pcapy-ng",
+        "pypacker",
+        "pyopenssl",
+        "elasticsearch",
+        "tabulate",
+    ],
+    entry_points={
+        "console_scripts": [
+            "dshell-decode = dshell.decode:main_command_line",
+        ],
+        "dshell_plugins": [],
+    },
+    scripts=[
+        "scripts/dshell",
+    ],
+)
 
 
 import os
 import sys
 import threading
-import subprocess
 from subprocess import Popen, PIPE
 from urllib.request import urlretrieve
 from urllib.parse import urlparse
 import venv
-from setuptools import setup, find_packages, Extension
 from build import BuildConfig
 
 # ExtendedEnvBuilder Class
